bc436d5822b331f22513e7ca9e06f5189087e1fb
Felipe Teles <felipersteles@gmail.com>
2023-03-23 11:46:30 -0300

    add: chessexception and chessposition

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,7 +1,6 @@
 package chess;
 
 import board.Board;
-import board.Position;
 import chess.pieces.King;
 import chess.pieces.Rook;
 
@@ -12,7 +11,7 @@ public class ChessMatch {
 
     public ChessMatch() {
         board = new Board(8, 8);
-        placePieces();
+        initialSetup();
     }
 
     // as peças do tipo Piece não são retornadas e sim
@@ -21,18 +20,22 @@ public class ChessMatch {
     public ChessPiece[][] getPieces() {
         ChessPiece[][] matrix = new ChessPiece[board.getRows()][board.getColumns()];
 
-        for(int i=0; i<board.getRows(); i++)    {
-            for(int j=0; j<board.getColumns(); j++) {   
-                matrix[i][j] = (ChessPiece) board.piece(i,j);
+        for (int i = 0; i < board.getRows(); i++) {
+            for (int j = 0; j < board.getColumns(); j++) {
+                matrix[i][j] = (ChessPiece) board.piece(i, j);
             }
         }
 
         return matrix;
     }
 
-    private void placePieces(){
-        board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
-        board.placePiece(new King(board, Color.BLACK), new Position(0, 4));
-        board.placePiece(new King(board, Color.WHITE), new Position(7, 4));
+    private void placeNewPiece(char column, int row, ChessPiece piece) {
+        board.placePiece(piece, new ChessPosition(column, row).toPosition());
+    }
+
+    private void initialSetup() {
+        placeNewPiece('b', 6, new Rook(board, Color.WHITE));
+        placeNewPiece('e',8,new King(board, Color.BLACK));
+        placeNewPiece('e',1,new King(board, Color.WHITE));
     }
 }
---
+++ src/chess/ChessPosition.java
@@ -0,0 +1,45 @@
+package chess;
+
+import board.Position;
+
+public class ChessPosition {
+    private char column;
+    private int row;
+
+    public ChessPosition(char column, int row) {
+        if (column < 'a' || column > 'h' || row < 0 || row > 8)
+            throw new ChessException("Erro ao estaciar posição. As possibilidades são de a1 a h8.");
+
+        this.column = column;
+        this.row = row;
+    }
+
+    public char getColumn() {
+        return this.column;
+    }
+
+    public void setColumn(char column) {
+        this.column = column;
+    }
+
+    public int getRow() {
+        return this.row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    protected Position toPosition() {
+        return new Position(8 - row, column - 'a');
+    }
+
+    protected static ChessPosition fromPosition(Position pos){
+        return new ChessPosition((char)('a' - pos.getColumn()), 8 - pos.getRow());
+    }
+
+    @Override
+    public String toString() {
+        return "" + column + row;
+    }
+}
