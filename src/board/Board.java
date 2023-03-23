6c6b978ef71dc8d79cd17e9c51eef7a385d60eea
Felipe Teles <felipersteles@gmail.com>
2023-03-23 11:28:16 -0300

    add: board exception and defensive programming

--- src/board/Board.java
+++ src/board/Board.java
@@ -6,6 +6,8 @@ public class Board {
     private Piece[][] pieces; // matrix de peças
 
     public Board(int rows, int columns) {
+        if (rows < 1 || columns < 1)
+            throw new BoardException("Erro ao criar tabuleiro: é necessário pelo menos 1 linha e 1 coluna");
         this.rows = rows;
         this.columns = columns;
         pieces = new Piece[rows][columns];
@@ -15,27 +17,14 @@ public class Board {
         return this.rows;
     }
 
-    @Override
-    public String toString() {
-        return "{" +
-            " rows='" + getRows() + "'" +
-            ", columns='" + getColumns() + "'" +
-            "}";
-    }
-
-    public void setRows(int rows) {
-        this.rows = rows;
-    }
-
     public int getColumns() {
         return this.columns;
     }
 
-    public void setColumns(int columns) {
-        this.columns = columns;
-    }
-
     public Piece piece(int row, int column) {
+        if (!positionExists(row, column))
+            throw new BoardException("Posição não encontrada");
+
         return pieces[row][column];
     }
 
@@ -44,6 +33,7 @@ public class Board {
     }
 
     public void placePiece(Piece piece, Position pos) {
+        if(thereIsAPiece(pos)) throw new BoardException("Já existe uma pela nessa posição: " + pos);
         pieces[pos.getRow()][pos.getColumn()] = piece;
 
         // esse acesso direto só é possivel
@@ -51,5 +41,29 @@ public class Board {
         // como board esta no mesmo pacote
         // ele tem acesso.
         piece.position = pos;
+    }
+
+    private boolean positionExists(int row, int column) {
+        // condições:
+        // row >= 0 and row < rows
+        // column >= 0 and column < columns
+        return row >= 0 && row < rows && column >= 0 && column < columns;
+    }
+
+    public boolean positionExists(Position pos) {
+        return positionExists(pos.getRow(), pos.getColumn());
+    }
+
+    public boolean thereIsAPiece(Position pos) {
+        if(!positionExists(pos)) throw new BoardException("Posição não encontrada");
+        return piece(pos) != null;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                " rows='" + getRows() + "'" +
+                ", columns='" + getColumns() + "'" +
+                "}";
     }
 }
